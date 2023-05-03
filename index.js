384f3a8b2d4c07fb7e1a1f1892b41c516781f320
Guilherme G. Silva <dev.gsilv@gmail.com>
2023-05-03 19:09:24 -0300

    [feat] API with Express

---
+++ index.js
@@ -0,0 +1,22 @@
+/*
+Mentoria 2023
+Api with framework
+*/
+
+const express = require('express');
+const server = express();
+const movies = require('./src/data/movies.json');
+
+server.get('/', (req, res) => {
+    return res.json({'main': 'index page'})
+});
+server.get('/users', (req, res) => {
+    return res.json({'user': 'admin'})
+});
+server.get('/movies', (req, res) => {
+    return res.json(movies)
+});
+
+server.listen(3000, () => {console.log("Server is running on port 3000")});
+
+
